9053c77c5fa63703ef29a1ad3bfa907520850087
Ben Kiggen and William Wittenbrock <william@>
2018-08-08 16:46:49 -0700

    Add javascript that creates address book cards.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
-$(document).ready(function(event){
+$(document).ready(function(){
+  $("form#newContact").submit(function(event){
+    event.preventDefault();
+    var firstName = $("#first").val();
+    var lastName = $("#last").val();
+    var phone = $("#phone").val();
+    var email = $("#email").val();
+
+    $("#firstNameCol").append('<div class="sizer"><p>' + '<span class="bold">' + firstName + '</span>' + '<br/>' + lastName + '<br/>' + phone + '<br/>' + email + '</p></div>');
+
+  });
+
+
+
 
 
 
